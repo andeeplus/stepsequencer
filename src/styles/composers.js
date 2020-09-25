9e3d89b5d1fe7aea0685d8de535cdbca599754ff
Andrea Zanenghi <andeeplus@gmail.com>
2020-09-25 21:02:13 +0200

    Electron added to app

--- src/styles/composers.js
+++ src/styles/composers.js
@@ -14,7 +14,7 @@ import {
 
 const composers = {
   box: compose(flexbox, layout, space, color, border, position, background, shadow, typography, grid),
-  text: compose(typography, color, space, layout, position, flexbox),
+  text: compose(typography, color, space, layout, border, position, flexbox),
 };
 
 export default composers;
